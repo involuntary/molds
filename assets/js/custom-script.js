8f6287d6b37892c69b452bf7bc6917da596c75fa
ram@codepixelz <ram@codepixelzmedia.com.np>
2017-02-16 19:58:48 +0545

    slicing files added

---
+++ assets/js/custom-script.js
@@ -0,0 +1,8 @@
+jQuery(document).ready(function($){
+    $('#chart-table').stacktable();
+    $('#table-1').stacktable();
+    $('#table-2').stacktable();    
+    $('#table-3').stacktable();
+    $('#table-4').stacktable();
+    $('#table-5').stacktable();
+});
